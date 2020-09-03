717c2ca3f3a6eb7fcc11c9168434f7ddabfe0a84
saloev <saloev.saadi@yandex.ru>
2020-09-04 02:05:17 +0300

    feat: chat ui

--- ui/src/components/chat/ChatMessages.vue
+++ ui/src/components/chat/ChatMessages.vue
@@ -1,21 +1,27 @@
 <template>
-  <ol class="chat-messages ma-0 pa-0">
-    <li v-for="item in list" :key="item.id" class="chat-messages__item mb-5">
-      <Message :item="item" :itself="item.itself" />
-    </li>
-  </ol>
+  <div class="chat-messages">
+    <ol class="chat-messages__list ma-0 pa-0">
+      <li v-for="item in list" :key="item.id" class="chat-messages__item mb-5">
+        <Message :item="item" :itself="item.itself" />
+      </li>
+    </ol>
+
+    <SendForm class="chat-messages__form" />
+  </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator';
 
 import Message from './ChatMessage.vue';
+import SendForm from './ChatMessagesSend';
 
 import { formatDate } from '@/utils/date';
 
 @Component({
   components: {
     Message,
+    SendForm,
   },
 })
 export default class ChatMessages extends Vue {
@@ -32,9 +38,23 @@ export default class ChatMessages extends Vue {
 
 <style lang="scss">
 .chat-messages {
-  list-style: none;
+  position: relative;
+  padding-bottom: 120px;
 
-  &__item {
+  &__list {
+    list-style: none;
+    max-height: calc(100vh - 500px);
+    overflow-y: auto;
+  }
+
+  &__form {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+
+    width: 100%;
+    padding: 10px;
+    border-radius: 10px;
   }
 }
 </style>
--- ui/src/components/chat/ChatMessagesSend.vue
+++ ui/src/components/chat/ChatMessagesSend.vue
@@ -1,10 +1,46 @@
-<template></template>
+<template>
+  <v-form ref="form" lazy-validation v-model="form" class="chat__form chat-form">
+    <v-textarea
+      @keyup.enter="submit"
+      required
+      v-model="message"
+      class="mx-2"
+      label="say hi"
+      rows="1"
+      :rules="messageRules"
+    ></v-textarea>
+    <v-btn class="mx-2" fab @click="submit">
+      <v-icon>mdi-send</v-icon>
+    </v-btn>
+  </v-form>
+</template>
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator';
 
 @Component
-export default class ChatMessagesSend extends Vue {}
+export default class ChatMessagesSend extends Vue {
+  message: string = '';
+  messageRules: any = [(v) => !!v || 'Message is required'];
+  form: boolean = false;
+
+  validate() {
+    //@ts-ignore
+    return this.$refs.form.validate();
+  }
+
+  submit() {
+    const isValid = this.validate();
+    if (!isValid) return;
+    this.$emit('sendMessage', { message: this.message });
+  }
+}
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.chat-form {
+  display: grid;
+  grid-template-columns: 1fr 60px;
+  align-items: center;
+}
+</style>
