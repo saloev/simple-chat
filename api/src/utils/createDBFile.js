f5488e0343780339ff4c60f738240626398d45af
saloev <saloev.saadi@yandex.ru>
2020-09-04 13:48:37 +0300

    fix: create directory for first time run server API

--- api/src/utils/createDBFile.js
+++ api/src/utils/createDBFile.js
@@ -2,6 +2,10 @@ import fs from "fs";
 import DBStructure from "../models/DBStructure.js";
 
 function fileHandler() {
+  fs.mkdirSync(process.cwd() + '/db/', {recursive: true}, (err) => {
+    if (err) throw err;
+    console.log("Directory created");
+  })
   fs.open("db/db.json", "w", (err) => {
     if (err) throw err;
     console.log("File created");
